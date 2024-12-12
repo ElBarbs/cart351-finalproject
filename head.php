e01e0862d6325593c37c76c367f002e385a4fc49
Louis B. <47404526+ElBarbs@users.noreply.github.com>
2024-12-12 17:48:48 -0500

    Fix favicon path in head.php to use dynamic base path.

--- head.php
+++ head.php
@@ -8,8 +8,10 @@
         href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="style.css" />
-    <link rel="icon" type="image/png" href="/assets/favicon.png" />
     <?php
+    $basePath = dirname($_SERVER['PHP_SELF']);
+    echo `<link rel="icon" type="image/png" href="$basePath/assets/favicon.png" />`;
+
     $currentPage = basename($_SERVER['PHP_SELF']);
     if ($currentPage === 'index.php') {
         echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>';
