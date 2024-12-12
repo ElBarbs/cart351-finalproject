e21dad3a1f2be67ef6d2286652bf5a2ca9551d34
Louis B. <47404526+ElBarbs@users.noreply.github.com>
2024-12-12 17:36:09 -0500

    Refactor HTML structure by moving head content to a separate head.php file and added favicon.

---
+++ head.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CART351 - Final Project</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/png" href="/assets/favicon.png" />
+    <?php
+    $currentPage = basename($_SERVER['PHP_SELF']);
+    if ($currentPage === 'index.php') {
+        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>';
+        echo '<script type="module" src="main.js"></script>';
+    }
+    ?>
+</head>
