d3e68443c2ab537cc858023940d9485161172b86
Louis B. <47404526+ElBarbs@users.noreply.github.com>
2024-12-12 19:15:08 -0500

    Enhance path.php to correctly construct the base path with trailing slash and forward slashes

--- path.php
+++ path.php
@@ -1,9 +1,18 @@
 <?php
 
-// Construct the full URL.
+// Get origin.
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $host = $_SERVER['HTTP_HOST'];
+
+// Get path.
 $path = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+
+// Replace backslashes with forward slashes.
 $path = str_replace("\\", "/", $path);
+
+// Add a trailing slash if it doesn't exist.
+if (substr($path, -1) !== '/') {
+    $path .= '/';
+}
 
 $BASE_PATH = $protocol . $host . $path;
